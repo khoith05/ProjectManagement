6b47fe3cccd9fb89449d020c6122194981905bdd
KHOI <t.hoangkhoi312@gmail.com>
2021-11-15 21:15:48 +0700

    ================Khoi=========== Add bottom navigation bar

--- app/src/main/java/com/example/android/projectmanagement/MainActivity.java
+++ app/src/main/java/com/example/android/projectmanagement/MainActivity.java
@@ -4,11 +4,49 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+
+import java.io.*;
+
+public class MainActivity extends AppCompatActivity {
+    Project project= new Project();
+    User user = new User();
+    Employee employee = new Employee();
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView= findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.user:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, user).commit();
+                        return true;
+
+                    case R.id.project:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, project).commit();
+                        return true;
+
+                    case R.id.employee:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, employee).commit();
+                        return true;
+                }
+                return false;
+            }
+        });
+        bottomNavigationView.setSelectedItemId(R.id.user);
+
+
     }
+
+
 }
